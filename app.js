2c189aee9b05b1580384f1146676a3385f60839c
LALedesma007 <laledesma@hotmail.com.ar>
2023-02-18 02:28:09 -0300

    Version 0.0.4

---
+++ app.js
@@ -0,0 +1,36 @@
+//-----------CARD IMAGEN DEL DIA ----------//
+
+const fecha = document.getElementById("date")
+const boton = document.getElementById("serch")
+const tarjeta = document.getElementById("card")
+const inicio = document.getElementById("volver")
+
+const crearCard = async() => {
+ let dia = fecha.value 
+  return(
+      await fetch(`https://api.nasa.gov/planetary/apod?api_key=GsZLTgRcMDlvoggvyKM3ouwwmiUwQnpSrBlogpSR&date=${dia}`)
+      .then(Response => Response.json())
+      .then(json => {
+        tarjeta.innerHTML = `
+<div class="row">
+  <div class="col-lg-7">
+    <img src="${json.url}" class="cardImg img-fluid rounded-start" alt="...">
+  </div>
+  <div class="col-lg-5">
+    <div class="card-body">
+      <h5 class="card-title text-center">${json.title}</h5>
+      <p class="card-text mt-3">${json.explanation}</p>
+    </div>
+  </div>
+</div>`;
+      })
+    )
+  } 
+  
+const reiciciarCard = () =>{
+ window.location.href="./"
+}
+  boton.addEventListener(("click") , crearCard)
+  inicio.addEventListener(("click"), reiciciarCard)
+
+//----------------------------------------------//
